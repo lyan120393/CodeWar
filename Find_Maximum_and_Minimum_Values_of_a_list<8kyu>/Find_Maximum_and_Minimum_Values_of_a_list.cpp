9270410a618969552bbdfc655ae3f45806399903
LY <lyan120393@troy.edu>
2018-02-21 10:12:28 -0600

    Add Find Max and Min to Origin, and then use codeanywhere to edit it. because Coderunner didn't support C++ 11, Vector cannot add {1,2,3,4,5}

---
+++ Find_Maximum_and_Minimum_Values_of_a_list<8kyu>/Find_Maximum_and_Minimum_Values_of_a_list.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> test;
+vector<int> numbers { 10, 20, 30, 40 };
+
+int min(vector<int> list){
+	
+	return list[0];
+}
+
+
+int max(vector<int> list){
+	
+	return list[0];
+}
+
+
+
+int main() {
+	
+}
+
+
+
