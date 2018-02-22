3731c094b083e491fedd54768bc2fd562ea1f102
LY <lyan120393@troy.edu>
2018-02-22 10:22:41 -0600

    Today I solve the first algorithm question, which is find max and min in a vector.

--- Find_Maximum_and_Minimum_Values_of_a_list<8kyu>/Find_Maximum_and_Minimum_Values_of_a_list.cpp
+++ Find_Maximum_and_Minimum_Values_of_a_list<8kyu>/Find_Maximum_and_Minimum_Values_of_a_list.cpp
@@ -2,25 +2,49 @@
 #include <vector>
 using namespace std;
 
-vector<int> test;
-vector<int> numbers { 10, 20, 30, 40 };
+vector<int> v {1, 2, 3, 4, 5, 20, 50 ,100 ,2200, 11002, 210301, 29310 , 31, 1231, 1319313011, -212};
 
-int min(vector<int> list){
-	
-	return list[0];
-}
-
-
-int max(vector<int> list){
-	
-	return list[0];
-}
+//Function Prototype
+int max(vector<int>);
+int min(vector<int>);
 
 
 
 int main() {
-	
+	cout << max(v) << endl;
+	cout << min(v) << endl;
+	return 0;
 }
 
+int min(vector<int> list){
+	int smallestnumber = 0;
+	bool isSet = false;
+	smallestnumber = list[0];
+	isSet = true;	
+	for (int i = 0;i < list.size();i++) {
+	
+		if (list[i] < smallestnumber && isSet == true ) {
+			smallestnumber = list[i];
+		}
+	}
+	return smallestnumber;
+}
+
+
+int max(vector<int> list){
+	int largestnumber = 0;
+	bool isSet = false;
+	largestnumber = list[0];
+	isSet = true;
+	for (int i = 0;i < list.size();i++) {
+		
+		if (list[i] > largestnumber && isSet == true ) {
+			largestnumber = list[i];
+		}
+
+
+	}
+	return largestnumber;
+}
 
 
