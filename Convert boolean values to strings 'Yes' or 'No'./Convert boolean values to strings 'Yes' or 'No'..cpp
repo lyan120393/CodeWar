020f85bbf0d8cc10bf4f425613bf6e35aa19006f
LY <lyan120393@troy.edu>
2018-02-27 11:14:50 -0600

    Really easy bool question

---
+++ Convert boolean values to strings 'Yes' or 'No'./Convert boolean values to strings 'Yes' or 'No'..cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+}
+
+string bool_to_word(bool value)
+{
+	if (value == true) {
+		return "Yes";
+	}else {
+		return "No";
+	}
+}
---
+++ Convert boolean values to strings 'Yes' or 'No'./Question.md
@@ -0,0 +1,5 @@
+Complete the bool_to_word (PHP: boolToWord ) method.
+
+Given: a boolean value
+
+Return: a 'Yes' string for true and a 'No' string for false
